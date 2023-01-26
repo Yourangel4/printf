3e42a4e1202eb1047f9c2379f4fe277abb4e04ff
Boluwatife Adeyemi <boluwatifeadeyemi122@gmail.com>
2023-01-26 11:31:44 -0800

    updated

--- functions1.c
+++ functions1.c
@@ -21,7 +21,7 @@ int print_unsigned(va_list types, char buffer[], int flags, int width,
 	num = convert_size_unsgnd(num, size);
 
 	if (num == 0)
-		buffer[i++] = '0';
+		buffer[i--] = '0';
 
 	buffer[BUFF_SIZE - 1] = '\0';
 
