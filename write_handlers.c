78511ce3a51074e4de5de75abb9ac84f2121752b
Boluwatife Adeyemi <boluwatifeadeyemi122@gmail.com>
2023-01-26 07:54:16 -0800

    updated

--- write_handlers.c
+++ write_handlers.c
@@ -13,11 +13,10 @@
  * Return: Number of chars printed.
  */
 int handle_write_char(char c, char buffer[],
-		int flags, int width, int precision, int size)
+	int flags, int width, int precision, int size)
 { /* char is stored at left and paddind at buffer's right */
 	int i = 0;
 	char padd = ' ';
-
 
 	UNUSED(precision);
 	UNUSED(size);
@@ -41,6 +40,7 @@ int handle_write_char(char c, char buffer[],
 			return (write(1, &buffer[BUFF_SIZE - i - 1], width - 1) +
 					write(1, &buffer[0], 1));
 	}
+
 	return (write(1, &buffer[0], 1));
 }
 
@@ -58,7 +58,7 @@ int handle_write_char(char c, char buffer[],
  * Return: Number of chars printed.
  */
 int write_number(int is_negative, int ind, char buffer[],
-		int flags, int width, int precision, int size)
+	int flags, int width, int precision, int size)
 {
 	int length = BUFF_SIZE - ind - 1;
 	char padd = ' ', extra_ch = 0;
@@ -75,7 +75,7 @@ int write_number(int is_negative, int ind, char buffer[],
 		extra_ch = ' ';
 
 	return (write_num(ind, buffer, flags, width, precision,
-				length, padd, extra_ch));
+		length, padd, extra_ch));
 }
 
 /**
@@ -92,8 +92,8 @@ int write_number(int is_negative, int ind, char buffer[],
  * Return: Number of printed chars.
  */
 int write_num(int ind, char buffer[],
-		int flags, int width, int prec,
-		int length, char padd, char extra_c)
+	int flags, int width, int prec,
+	int length, char padd, char extra_c)
 {
 	int i, padd_start = 1;
 
@@ -103,10 +103,8 @@ int write_num(int ind, char buffer[],
 		buffer[ind] = padd = ' '; /* width is displayed with padding ' ' */
 	if (prec > 0 && prec < length)
 		padd = ' ';
-
 	while (prec > length)
 		buffer[--ind] = '0', length++;
-
 	if (extra_c != 0)
 		length++;
 	if (width > length)
@@ -125,7 +123,6 @@ int write_num(int ind, char buffer[],
 			if (extra_c)
 				buffer[--ind] = extra_c;
 			return (write(1, &buffer[1], i - 1) + write(1, &buffer[ind], length));
-
 		}
 		else if (!(flags & F_MINUS) && padd == '0')/* extra char to left of padd */
 		{
@@ -137,10 +134,8 @@ int write_num(int ind, char buffer[],
 	}
 	if (extra_c)
 		buffer[--ind] = extra_c;
-
 	return (write(1, &buffer[ind], length));
 }
-
 
 /**
  * write_unsgnd - Writes an unsigned number
@@ -154,12 +149,11 @@ int write_num(int ind, char buffer[],
  *
  * Return: Number of written chars.
  */
-
 int write_unsgnd(int is_negative, int ind,
-		char buffer[],
-		int flags, int width, int precision, int size)
+	char buffer[],
+	int flags, int width, int precision, int size)
 {
-	/*The number is stored at the bufer's right and starts at position i*/
+	/* The number is stored at the bufer's right and starts at position i */
 	int length = BUFF_SIZE - ind - 1, i = 0;
 	char padd = ' ';
 
@@ -177,6 +171,7 @@ int write_unsgnd(int is_negative, int ind,
 		buffer[--ind] = '0';
 		length++;
 	}
+
 	if ((flags & F_ZERO) && !(flags & F_MINUS))
 		padd = '0';
 
@@ -184,6 +179,7 @@ int write_unsgnd(int is_negative, int ind,
 	{
 		for (i = 0; i < width - length; i++)
 			buffer[i] = padd;
+
 		buffer[i] = '\0';
 
 		if (flags & F_MINUS) /* Asign extra char to left of buffer [buffer>padd]*/
@@ -212,9 +208,8 @@ int write_unsgnd(int is_negative, int ind,
  *
  * Return: Number of written chars.
  */
-
 int write_pointer(char buffer[], int ind, int length,
-		int width, int flags, char padd, char extra_c, int padd_start)
+	int width, int flags, char padd, char extra_c, int padd_start)
 {
 	int i;
 
@@ -246,7 +241,7 @@ int write_pointer(char buffer[], int ind, int length,
 			buffer[1] = '0';
 			buffer[2] = 'x';
 			return (write(1, &buffer[padd_start], i - padd_start) +
-					write(1, &buffer[ind], length - (1 - padd_start) - 2));
+				write(1, &buffer[ind], length - (1 - padd_start) - 2));
 		}
 	}
 	buffer[--ind] = 'x';
@@ -255,4 +250,3 @@ int write_pointer(char buffer[], int ind, int length,
 		buffer[--ind] = extra_c;
 	return (write(1, &buffer[ind], BUFF_SIZE - ind - 1));
 }
-
